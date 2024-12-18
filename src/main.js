1d162f7bfb040559cb16eec21ef3e983ed3a9365
Ranzil Castelino <ranzil1409@gmail.com>
2024-12-18 16:09:01 +0530

    Fix sidebar toggle functionality

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
-import './style.css'
+const toggleswitch = document.getElementById('toggleswitch');
+const hamburger = document.querySelector(".hamburger");
+const sidecontent = document.querySelector(".sidecontent");
+
+toggleswitch.addEventListener("click",()=>{
+document.body.classList.toggle("light-theme");
+});
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('larrow');
+    sidecontent.classList.toggle('show');
+});
 
